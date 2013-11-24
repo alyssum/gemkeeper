b6d0d25ea14c153ddd65448fe291efbd030ad517
Alissa Harrison <alissaha@amazon.com>
2013-11-24 14:47:57 -0800

    Using scope block for order to avoid deprecation warning

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
   has_many :sessions
   has_many :participations, through: :sessions
   has_many :players, through: :participations
-  has_many :roles, :dependent => :delete_all, :order => "name"
+  has_many :roles, -> { order("name") }, :dependent => :delete_all
 
   validates :name, presence: true
   validates :max_players, numericality: { greater_than: 0 }
